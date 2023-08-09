d8a55f225620305258f91e344acee7ee5103e427
AnggunSafitri <anggunsafitri2211@gmail.com>
2023-08-09 15:07:06 +0700

    update ekku

---
+++ app/Models/User.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Support\Str;
+use App\Models\Model;
+use App\Models\ModelAuthenticate;
+
+class User extends ModelAuthenticate
+{
+    protected $table = "user";
+    
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($item) {
+            $item->id = (string) Str::orderedUuid();
+        });
+    }
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = bcrypt($value);
+    }
+
+    function handleUploadPoto()
+    {
+        if (request()->hasFile('poto')) {
+            $poto = request()->file('poto');
+            $destination = "user";
+            $randomStr = Str::random(5);
+            $filename = time() . "-"  . $randomStr . "."  . $poto->extension();
+            $url = $poto->storeAs($destination, $filename);
+            $this->poto = "app/" . $url;
+            
+
+        }
+    }
+
+    
+}
